4988b8d819b788b31e994897f403ea660324e1b9
Stephannie M <rumbi.m@hotmail.co.uk>
2024-03-15 21:58:47 +0000

    fix styling for home, chara and quiz page

--- paradise/urls.py
+++ paradise/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 
 from . import views
-urlpatterns = [ path('', views.index, name='index'), ]
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('characters/', views.characters, name='characters'),
+    path('quiz/', views.quiz, name='quiz')
+]
--- paradise/views.py
+++ paradise/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
 def index(request):
+    context = {}
     return render(request, 'paradise/index.html')
+
+def characters(request):
+    context = {}
+    return render(request, 'paradise/characters.html')
+
+def quiz(request):
+    context = {}
+    return render(request, 'paradise/quizgame.html')
