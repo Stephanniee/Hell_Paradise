a013a4d982d828d091b38102421db5ee0a846136
Stephannie M <rumbi.m@hotmail.co.uk>
2024-03-15 01:34:32 +0000

    modles update

--- paradise/models.py
+++ paradise/models.py
@@ -4,8 +4,9 @@ from cloudinary.models import CloudinaryField
 class Merch(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    image = CloudinaryField('image')
-    description = models.TextField()
+    description = models.TextField(default='')
+    image = CloudinaryField('image', default='placeholder')
+    
     def __str__(self):
         return self.name
 
